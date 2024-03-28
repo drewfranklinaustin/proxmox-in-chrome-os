7d429bd638ec6b7e2791df8096c0a7b15630fde8
drewfranklinaustin <60346310+drewfranklinaustin@users.noreply.github.com>
2024-03-27 20:33:57 -0500

    setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-sudo apt-get install wget nala nano gpg -f -y && sudo usermod -aG sudo drewfranklinaustin && sudo passwd root && sudo passwd drewfranklinaustin && wget -q -O - https://dl-ssl.google.com/linux>
-sudo apt-get build-dep fuse fuse3
-sudo dpkg -r  
+sudo apt-get install wget nala nano gpg -f -y && sudo usermod -aG sudo drewfranklinaustin && sudo passwd root && sudo passwd drewfranklinaustin && wget -q -O - https://dl-ssl.google.com/linux> && sudo apt-get build-dep fuse fuse3
+sudo dpkg -r fuse 
 sudo apt-get -f install
 sudo aptitude install fuse fuse3
